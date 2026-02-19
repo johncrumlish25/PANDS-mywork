4d27bd10cc0b2809ff8651d60ba8ff1719e48502
johncrumlish25 <johncrumlish16@gmail.com>
2026-02-19 19:39:08 +0000

    Created file evens.py

---
+++ week04/evens.py
@@ -0,0 +1,11 @@
+# evens.py
+# Uses a while loop to print all the even numbers from 2 to 100
+# Author: John Crumlish
+
+
+number_to = 100
+even_num = 2
+
+while even_num <= number_to:
+    print(even_num)
+    even_num +=2
